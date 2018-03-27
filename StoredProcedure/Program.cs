8132ed315c671079f0f97af2e84b0d37da7ec6e5
AndreeaOprea <deea.ouup@yahoo.com>
2018-03-27 09:17:41 +0200

    stored procedure example

--- StoredProcedure/Program.cs
+++ StoredProcedure/Program.cs
@@ -117,7 +117,6 @@ namespace StoredProcedure
                 cmd.CommandText = "call getcat(@1,@2,@3)";
 
                 var reader = cmd.ExecuteReader();
-
                 while (reader.Read())
                 {
                     Console.WriteLine("{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
